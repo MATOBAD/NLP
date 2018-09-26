87e3233a50591ed918b79bbba8865774a0a81b7e
MATOBAD <e1b15097@st.oit.ac.jp>
2018-09-26 02:41:29 -0700

    fix

--- common/util.py
+++ common/util.py
@@ -105,6 +105,21 @@ def ppmi(C, verbose=False, eps=1e-8):
     return M
 
 
+def create_contexts_target(corpus, window_size=1):
+    target = corpus[window_size:-window_size]
+    contexts = []
+
+    for idx in range(window_size, len(corpus)-window_size):
+        cs = []
+        for t in range(-window_size, window_size+1):
+            if t == 0:
+                continue
+            cs.append(corpus[idx + t])
+        contexts.append(cs)
+
+    return np.array(contexts), np.array(target)
+
+
 def main():
     text = 'You say goodbye and I say hello'
     corpus, word_to_id, id_to_word = preprocess(text)
