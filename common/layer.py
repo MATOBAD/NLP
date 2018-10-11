cb2e6b0b9ca6f7eebf9f3a63a6b5984ba2e7e42b
MATOBAD <e1b15097@st.oit.ac.jp>
2018-10-11 19:30:58 +0900

    add negative_sampling, SoftmaxWithLoss, Embedding

--- common/layer.py
+++ common/layer.py
@@ -68,3 +68,50 @@ class SoftmaxWithLoss:
         dx = dx / batch_size
 
         return dx
+
+
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None  # sigmoidの出力
+        self.t = None  # 教師データ
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+        return dx
+
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    # 順伝播
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    # 逆伝播
+    def backward(self, dout):
+        dW = self.grads
+        dW[...] = 0
+
+        for i, word_in in enumerate(self.idx):
+            np.add.at(dW, self.idx, dout)
+            # dW[word_id] += dout[i]
+
+        return None
