46c843ac0f47ee374915034c15a65f7faa467668
MATOBAD <e1b15097@st.oit.ac.jp>
2018-12-20 12:47:44 +0900

    add LSTM

--- common/time_layers.py
+++ common/time_layers.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
+from common.layers import *
+from common.functions import sigmoid
 
 
 class RNN:
@@ -237,3 +239,33 @@ class TimeSigmoidWithLoss:
             dxs[:, t] = layer.backward(dout)
 
         return dxs
+
+
+class LSTM:
+    def __init__(self, Wx, Wh, b):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.cache = None
+
+    def forward(self, x, h_prev, c_prev):
+        Wx, Wh, b = self.params
+        N, H = h_prev.shape
+
+        A = np.dot(x, Wx) + np.dot(h_prev, Wh) + b
+
+        # slice
+        f = A[:, :H]
+        g = A[:, H:2*H]
+        i = A[:, 2*H:3*H]
+        o = A[:, 3*H:]
+
+        f = sigmoid(f)
+        g = np.tanh(g)
+        i = sigmoid(i)
+        o = sigmoid(o)
+
+        c_next = f * c_prev + g * i
+        h_next = o * np.tanh(c_next)
+
+        self.cache = (x, h_prev, c_prev, i, f, g, o, c_next)
+        return h_next, c_next
